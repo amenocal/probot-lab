27b8c14f4145a3d369360e853aa76e507398b35e
Alejandro Menocal <amenocal@github.com>
2022-06-03 15:19:01 -0500

    yeah adding a comment on a comment it's an endless loop

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ module.exports = (app) => {
     return context.octokit.issues.createComment(issueComment);
 });
 
-  app.on("issue_comment.created", async (context) => {
-    const issueComment = context.issue({
-        body: "Thanks for your comment!", 
-    });
-    return context.octokit.issues.createComment(issueComment);
-});
 
   // For more information on building apps:
   // https://probot.github.io/docs/
